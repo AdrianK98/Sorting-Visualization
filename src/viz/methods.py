3ac6c04c4297aa49d9429711abdfde93a2e5c6e5
AdrianK98 <adrian.k.koczur@gmail.com>
2023-06-13 20:44:25 +0200

    feat: fix

--- src/viz/methods.py
+++
@@ -1,9 +0,0 @@
-from enum import Enum
-
-
-class Method(Enum):
-    BUBBLE_SORT = "bubbleSort"
-    MERGE_SORT = "mergeSort"
-    SELECTION_SORT = "selectionSort"
-    QUICK_SORT = "quickSort"
-    RADIX_SORT = "radixSort"
--- src/viz/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-class Values(models.Model):
-    valueList = models.TextField(null=True)
-
-    def __str__(self):
-        return str(self.id)
